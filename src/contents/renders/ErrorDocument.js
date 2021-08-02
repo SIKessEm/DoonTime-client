1c1efe72a9f5221bce56de25192e20522bf02239
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-08-03 00:51:01 +0200

    Use viewport height as height of 404 error document content

--- src/contents/renders/ErrorDocument.js
+++ src/contents/renders/ErrorDocument.js
@@ -21,7 +21,7 @@ class ErrorDocument extends React.Component {
   _404() {
     return (
       <Container fluid={true} className="bg-dark text-light">
-        <main className="d-flex flex-column justify-content-center align-items-center" style={{ height: 'calc(100vmin + 100vh - 100vmin)' }}>
+        <main className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
           <h1 className="mt-5 mb-2">Error document {this.props.code}</h1>
         <div className="text-center">
             <p className="lead">The requested content does not exist in our documents</p>
