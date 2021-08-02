c930b83d7e9b6a77645622441e36cd65e859ae29
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-08-03 00:42:08 +0200

    Add 404 document render

--- src/contents/renders/ErrorDocument.js
+++ src/contents/renders/ErrorDocument.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Container} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
 import '../designs/Document.scss'
 
 class ErrorDocument extends React.Component {
@@ -19,10 +21,11 @@ class ErrorDocument extends React.Component {
   _404() {
     return (
       <Container fluid={true} className="bg-dark text-light">
-        <main className="d-flex flex-column justify-content-center align-items-center">
+        <main className="d-flex flex-column justify-content-center align-items-center" style={{ height: 'calc(100vmin + 100vh - 100vmin)' }}>
           <h1 className="mt-5 mb-2">Error document {this.props.code}</h1>
           <div>
-            <p>Document not found</p>
+            <p className="lead">The requested content does not exist in our documents</p>
+            <p className="text-center"><Link exact to='/' className='btn btn-lg btn-light'>Go to home</Link></p>
           </div>
         </main>
       </Container>
