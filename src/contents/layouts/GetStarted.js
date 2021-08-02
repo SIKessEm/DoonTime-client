f14ea0a66c7efd3e6cbc6828e0b35129f7dce3d7
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-08-02 20:26:50 +0200

    Add the content of getting start

--- src/contents/layouts/GetStarted.js
+++ src/contents/layouts/GetStarted.js
@@ -1,13 +1,38 @@
 import {Fragment} from 'react'
+import {Link} from 'react-router-dom'
+import {Container, Form, Row, Col, FloatingLabel, Button} from 'react-bootstrap'
 
 export default function Home(){
   return (
     <Fragment>
-      <div className="cover-container pt-5 d-flex w-100 h-100 text-center justify-content-center flex-column">
+      <Container className="pt-5 d-flex w-100 h-100 text-center justify-content-center flex-column">
         <main>
           <h1 className="pb-3">Get started on DoonTime</h1>
+          <p className="lead"><Link exact to="/LogIn">Log in</Link> or <Link exact to="/SignUp">sign up for free</Link> to take advantage of Doontime</p>
+          <Form>
+            <Row className="flex-column">
+              <Col lg={{ span: 4, offset: 4 }} md={{ span: 6, offset: 3 }} sm={{ span: 8, offset: 2 }} xs={{ span: 10, offset: 1 }}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <FloatingLabel controlId="floatingInputGrid" label="Enter your username or email">
+                    <Form.Control type="email" placeholder="Enter your username or email" />
+                  </FloatingLabel>
+                  <Form.Text className="text-muted">
+                    We'll never share your username or email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="m-auto mb-3 content-fit" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Subscribe to the newsletter to stay informed for free" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">Continue &hellip;</Button>
+              </Col>
+            </Row>
+          </Form>
         </main>
-      </div>
+      </Container>
     </Fragment>
   )
 };
