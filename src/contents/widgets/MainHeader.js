338f86a7f41729619a062a7f6bf229f1c4fb6382
SIGUI kessé Emmanuel <sikessem@omninf.com>
2021-08-03 15:12:05 +0200

    Update the main logo animation style

--- src/contents/widgets/MainHeader.js
+++ src/contents/widgets/MainHeader.js
@@ -6,7 +6,7 @@ export default function MainHeader() {
     <Fragment>
       <div className="Main-header">
         <header>
-          <h1 className="h3 pt-3"><a className="Main-link" href="/" title={process.env.REACT_APP_WEBSITE_FULLNAME}><img src={logo} className="Main-logo" alt={process.env.REACT_APP_WEBSITE_SHORTNAME}/></a></h1>
+          <h1 className="h3 pt-5"><a className="Main-link" href="/" title={process.env.REACT_APP_WEBSITE_FULLNAME}><img src={logo} className="Main-logo" alt={process.env.REACT_APP_WEBSITE_SHORTNAME}/></a></h1>
         </header>
       </div>
     </Fragment>
